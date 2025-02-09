307c1c8573b46eebccc53b2b3974708e758df052
Sumit Kumar Saw <192447709+3x3cu73@users.noreply.github.com>
2025-02-09 14:37:15 +0530

    login

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'OCS' });
+  res.render('index', { title: 'OCS App' });
 });
 
 module.exports = router;
