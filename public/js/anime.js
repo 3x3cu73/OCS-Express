91b6094a13b702a7478d86f653467c8d00a83189
Sumit Kumar Saw <192447709+3x3cu73@users.noreply.github.com>
2025-02-09 14:49:11 +0530

    network error

--- public/js/anime.js
+++ public/js/anime.js
@@ -58,10 +58,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
             }
 
-            // else{
-            //     document.getElementById("error").innerHTML =error;
-            //     document.getElementById("success").innerHTML ="";
-            // }
+            else{
+                document.getElementById("error").innerHTML =error;
+                document.getElementById("success").innerHTML ="";
+            }
 
         }
 
